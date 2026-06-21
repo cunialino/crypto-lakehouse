facc15f165399951bc23335bfdf514055ded2442
Elia Cunial <elia.cunial@gmail.com>
2026-06-21 21:36:14 +0200

    feat(rs-batch): crypto batcher init

--- rustapps/crypto-batcher/src/main.rs
+++ rustapps/crypto-batcher/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use crypto_data::data::binance_web_socket::TradeEventBinance;
+use serde::Deserialize;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> anyhow::Result<()> {
+    // let file = File::open("BTCUSDT-trades-2026-04-29.csv")?;
+    let file = File::open("test.csv")?;
+    let buf_lines = io::BufReader::new(file).lines();
+    for line in buf_lines.map_while(Result::ok) {
+        println!("{}", line);
+    }
+    Ok(())
 }
