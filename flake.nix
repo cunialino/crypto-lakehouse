ce25008f0f83763cbfc327b9046feef00b869e33
Elia Cunial <elia.cunial@gmail.com>
2026-06-25 10:41:21 +0200

    feat(flake): add spark

--- flake.nix
+++ flake.nix
@@ -29,6 +29,71 @@
         nativeBuildInputs = [
           pkgs.pkg-config
         ];
+        python = pkgs.python314.withPackages (ps: [
+          ps.py4j
+        ]);
+        icebergVersion = "1.11.0";
+        sparkCompat = "4.1";
+        scalaVersion = "2.13";
+        hadoopVersion = "3.4.2";
+        awsSdkV2Version = "2.29.52";
+
+        extraJars = [
+          (pkgs.fetchurl {
+            url = "https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-spark-runtime-${sparkCompat}_${scalaVersion}/${icebergVersion}/iceberg-spark-runtime-${sparkCompat}_${scalaVersion}-${icebergVersion}.jar";
+            sha256 = "sha256-1upsXQmSiNrrfVqSBhvT19jylkkmMrQjeOXy8OMGYkI=";
+          })
+          (pkgs.fetchurl {
+            url = "https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-aws/${icebergVersion}/iceberg-aws-${icebergVersion}.jar";
+            sha256 = "sha256-TP5Ke6Ok+yR6KeYMYUZ4w7eh6bS5H1TgyjQGQZyTOPA=";
+          })
+          (pkgs.fetchurl {
+            url = "https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/${hadoopVersion}/hadoop-aws-${hadoopVersion}.jar";
+            sha256 = "sha256-0tJOa3FbVla50ji/E7ZoKgfiAE3Taq10pzrrhUMZ9Nk=";
+          })
+          (pkgs.fetchurl {
+            url = "https://repo1.maven.org/maven2/software/amazon/awssdk/bundle/${awsSdkV2Version}/bundle-${awsSdkV2Version}.jar";
+            sha256 = "sha256-YmOPMwRWkMa8XklrVLCy6Jqa04y+2s324Bf+pryF9Vw=";
+          })
+        ];
+
+        spark = pkgs.stdenv.mkDerivation rec {
+          pname = "spark";
+          version = "4.1.2";
+
+          src = pkgs.fetchzip {
+            url = "https://downloads.apache.org/spark/spark-${version}/spark-${version}-bin-hadoop3.tgz";
+            sha256 = "sha256-cI22md2A0Hy0bPqIiD7wO5MKQBxDiEOKAWEOHuBSfls=";
+          };
+
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r . $out/
+
+            patchShebangs $out/bin
+
+            for jar in ${pkgs.lib.concatStringsSep " " (map toString extraJars)}; do
+              cp "$jar" "$out/jars/$(stripHash "$jar")"
+            done
+
+            for f in $out/bin/*; do
+              if [ -f "$f" ] && [ -x "$f" ]; then
+                wrapProgram "$f" \
+                --set JAVA_HOME "${pkgs.jdk21}" --set SPARK_HOME "$out" \
+                --set PYSPARK_PYTHON "${python}/bin/python3" \
+                --set PYSPARK_DRIVER_PYTHON "${python}/bin/python3"
+              fi
+            done
+          '';
+
+          meta = {
+            description = "Apache Spark ${version}";
+            homepage = "https://spark.apache.org/";
+            license = pkgs.lib.licenses.asl20;
+          };
+        };
       in
       let
         cryptoCollector = pkgs.rustPlatform.buildRustPackage {
@@ -74,17 +139,21 @@
           mkShell {
             nativeBuildInputs = [
               pkgs.pkg-config
-              pkgs.python3
             ];
             env = {
-              UV_PYTHON = "${pkgs.python3}";
+              UV_PYTHON = "${python}";
             };
             buildInputs = [
               rustToolchain
+              spark
               pkgs.rust-analyzer
               pkgs.protobuf
               pkgs.openssl
             ];
+            shellHook = ''
+              export SPARK_LOCAL_IP=127.0.0.1
+              export PYTHONPATH="''${PYTHONPATH:+$PYTHONPATH:}${spark}/python:${python}/lib/python3.14/site-packages"
+            '';
           };
       }
     );
