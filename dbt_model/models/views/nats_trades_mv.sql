88b5546fe8852d886ecd8ee030947168ea0868a7
Elia Cunial <elia.cunial@gmail.com>
2026-09-07 17:49:58 +0200

    chore(cleanup): delete risingwave dbt models

--- dbt_model/models/views/nats_trades_mv.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='materialized_view') }}
-
-SELECT DISTINCT ON (exchange, symbol, trade_id) *
-FROM {{ ref('nats_trades') }}
-WHERE trade_ts > CURRENT_TIMESTAMP - INTERVAL '4 days'
