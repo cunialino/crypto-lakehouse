7ba7d312b70010b58b61d58effe604c83d32b7e8
Elia Cunial <elia.cunial@gmail.com>
2026-09-07 18:40:40 +0200

    fix(arroyo): trade level gaps

--- deploy/base/arroyo/sql/gaps.sql
+++ deploy/base/arroyo/sql/gaps.sql
@@ -1,15 +1,12 @@
 -- checkpoint_interval_micros: 60000000
 INSERT INTO trade_gaps_sink
-SELECT exchange, symbol, window.start AS window_start,
-       min_trade_id, max_trade_id, events, skipped
+SELECT exchange, symbol, trade_id, next_trade_id - trade_id - 1 AS skipped
 FROM (
-  SELECT tumble(interval '1 minute') AS window,
-         exchange, symbol,
-         cast(min(trade_id) as bigint) AS min_trade_id,
-         cast(max(trade_id) as bigint) AS max_trade_id,
-         count(*) AS events,
-         cast(max(trade_id) as bigint) - cast(min(trade_id) as bigint) + 1 - count(*) AS skipped
+  SELECT exchange, symbol, trade_id,
+         lead(trade_id) over (partition by tumble(interval '1 minute') order by trade_id) AS next_trade_id
   FROM nats_trades
-  GROUP BY window, exchange, symbol
+  WHERE cast(event_time as bigint) < cast(to_unixtime(now()) * 1000 - 60000 as bigint)
+  GROUP BY tumble(interval '1 minute'), exchange, symbol, trade_id
 )
-where skipped > 0
+WHERE next_trade_id IS NOT NULL
+  AND next_trade_id - trade_id > 1
